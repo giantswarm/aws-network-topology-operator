678b0126507117d89d21469a14edea0bda49fef1
Mario Nitchev <mario@giantswarm.io>
2023-01-17 20:40:22 +0200

    Add ShareReconciler

---
+++ controllers/share.go
@@ -0,0 +1,126 @@
+package controllers
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws/arn"
+	"github.com/go-logr/logr"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	capi "sigs.k8s.io/cluster-api/api/v1beta1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	"github.com/giantswarm/aws-network-topology-operator/pkg/aws"
+	"github.com/giantswarm/aws-network-topology-operator/pkg/util/annotations"
+)
+
+//counterfeiter:generate . RAMClient
+type RAMClient interface {
+	ApplyResourceShare(context.Context, aws.ResourceShare) error
+}
+
+type ShareReconciler struct {
+	ramClient     RAMClient
+	clusterClient ClusterClient
+}
+
+func NewShareReconciler(clusterClient ClusterClient, ramClient RAMClient) *ShareReconciler {
+	return &ShareReconciler{
+		ramClient:     ramClient,
+		clusterClient: clusterClient,
+	}
+}
+
+func (r *ShareReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&capi.Cluster{}).
+		Complete(r)
+}
+
+func (r *ShareReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	logger := r.getLogger(ctx)
+
+	logger.Info("Reconciling")
+	defer logger.Info("Done reconciling")
+
+	cluster, err := r.clusterClient.Get(ctx, req.NamespacedName)
+	if k8serrors.IsNotFound(err) {
+		logger.Info("cluster no longer exists")
+		return ctrl.Result{}, nil
+	}
+	if err != nil {
+		logger.Error(err, "failed to get cluster")
+		return ctrl.Result{}, err
+	}
+
+	return r.reconcileNormal(ctx, cluster)
+}
+
+func (r *ShareReconciler) reconcileNormal(ctx context.Context, cluster *capi.Cluster) (ctrl.Result, error) {
+	logger := r.getLogger(ctx)
+
+	transitGatewayAnnotation := annotations.GetNetworkTopologyTransitGatewayID(cluster)
+
+	if transitGatewayAnnotation == "" {
+		logger.Info("transit gateway arn annotation not set yet")
+		return ctrl.Result{}, nil
+	}
+
+	transitGatewayARN, err := arn.Parse(transitGatewayAnnotation)
+	if err != nil {
+		logger.Error(err, "failed to parse transit gateway arn")
+		return ctrl.Result{}, err
+	}
+
+	accountID, err := r.getAccountId(ctx, cluster)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	if accountID == transitGatewayARN.AccountID {
+		logger.Info("transit gateway id not set yet")
+		return ctrl.Result{}, nil
+	}
+
+	err = r.ramClient.ApplyResourceShare(ctx, aws.ResourceShare{
+		Name:              fmt.Sprintf("%s-transit-gateway", cluster.Name),
+		ResourceArns:      []string{transitGatewayARN.String()},
+		ExternalAccountID: accountID,
+	})
+	if err != nil {
+		logger.Error(err, "failed to apply resource share")
+		return ctrl.Result{}, err
+	}
+
+	return ctrl.Result{}, nil
+}
+
+func (r *ShareReconciler) getAccountId(ctx context.Context, cluster *capi.Cluster) (string, error) {
+	logger := r.getLogger(ctx)
+	awsCluster := types.NamespacedName{
+		Name:      cluster.Spec.InfrastructureRef.Name,
+		Namespace: cluster.Spec.InfrastructureRef.Namespace,
+	}
+
+	identity, err := r.clusterClient.GetAWSClusterRoleIdentity(ctx, awsCluster)
+	if err != nil {
+		logger.Error(err, "failed to get AWSCluster role identity")
+		return "", err
+	}
+
+	roleArn, err := arn.Parse(identity.Spec.RoleArn)
+	if err != nil {
+		logger.Error(err, "failed to parse aws cluster role identity arn")
+		return "", err
+	}
+
+	return roleArn.AccountID, nil
+}
+
+func (r *ShareReconciler) getLogger(ctx context.Context) logr.Logger {
+	logger := log.FromContext(ctx)
+	logger = logger.WithName("share-reconciler")
+	return logger
+}
---
+++ controllers/share_test.go
@@ -0,0 +1,258 @@
+package controllers_test
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	capa "sigs.k8s.io/cluster-api-provider-aws/api/v1beta1"
+	capi "sigs.k8s.io/cluster-api/api/v1beta1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/giantswarm/aws-network-topology-operator/controllers"
+	"github.com/giantswarm/aws-network-topology-operator/controllers/controllersfakes"
+	"github.com/giantswarm/aws-network-topology-operator/pkg/k8sclient"
+	"github.com/giantswarm/aws-network-topology-operator/pkg/util/annotations"
+	"github.com/giantswarm/aws-network-topology-operator/tests"
+)
+
+var _ = FDescribe("Share", func() {
+	var (
+		ctx context.Context
+
+		name              string
+		sourceAccountID   = "123456789012"
+		transitGatewayARN = fmt.Sprintf("arn:aws:ec2:eu-west-2:%s:transit-gateway/tgw-01234567890abcdef", sourceAccountID)
+		externalAccountID = "987654321098"
+
+		cluster         *capi.Cluster
+		clusterIdentity *capa.AWSClusterRoleIdentity
+		awsCluster      *capa.AWSCluster
+		request         ctrl.Request
+
+		ramClient  *controllersfakes.FakeRAMClient
+		reconciler *controllers.ShareReconciler
+	)
+
+	BeforeEach(func() {
+		ctx = context.Background()
+
+		name = tests.GenerateGUID("test")
+		cluster = &capi.Cluster{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: namespace,
+				Annotations: map[string]string{
+					annotations.NetworkTopologyTransitGatewayIDAnnotation: transitGatewayARN,
+				},
+			},
+			Spec: capi.ClusterSpec{
+				InfrastructureRef: &corev1.ObjectReference{
+					Kind:      "AWSCluster",
+					Namespace: namespace,
+					Name:      name,
+				},
+			},
+		}
+		err := k8sClient.Create(ctx, cluster)
+		Expect(err).NotTo(HaveOccurred())
+
+		clusterIdentity = &capa.AWSClusterRoleIdentity{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: name,
+			},
+			Spec: capa.AWSClusterRoleIdentitySpec{
+				AWSRoleSpec: capa.AWSRoleSpec{
+					RoleArn: fmt.Sprintf("arn:aws:iam::%s:role/the-role-name", externalAccountID),
+				},
+			},
+		}
+		err = k8sClient.Create(ctx, clusterIdentity)
+		Expect(err).NotTo(HaveOccurred())
+
+		awsCluster = &capa.AWSCluster{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: namespace,
+			},
+			Spec: capa.AWSClusterSpec{
+				IdentityRef: &capa.AWSIdentityReference{
+					Kind: "AWSClusterRoleIdentity",
+					Name: name,
+				},
+			},
+		}
+		err = k8sClient.Create(ctx, awsCluster)
+		Expect(err).NotTo(HaveOccurred())
+
+		request = ctrl.Request{
+			NamespacedName: types.NamespacedName{
+				Name:      cluster.Name,
+				Namespace: cluster.Namespace,
+			},
+		}
+
+		ramClient = new(controllersfakes.FakeRAMClient)
+		reconciler = controllers.NewShareReconciler(
+			k8sclient.NewCluster(k8sClient, types.NamespacedName{}),
+			ramClient,
+		)
+	})
+
+	It("shares the transit gateway", func() {
+		result, err := reconciler.Reconcile(ctx, request)
+
+		Expect(result.Requeue).To(BeFalse())
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(ramClient.ApplyResourceShareCallCount()).To(Equal(1))
+		_, resourceShare := ramClient.ApplyResourceShareArgsForCall(0)
+		Expect(resourceShare.Name).To(Equal(fmt.Sprintf("%s-transit-gateway", name)))
+		Expect(resourceShare.ResourceArns).To(ConsistOf(transitGatewayARN))
+		Expect(resourceShare.ExternalAccountID).To(Equal(externalAccountID))
+	})
+
+	When("the transit gateway hasn't been created yet", func() {
+		BeforeEach(func() {
+			patchedCluster := cluster.DeepCopy()
+			patchedCluster.Annotations[annotations.NetworkTopologyTransitGatewayIDAnnotation] = ""
+			err := k8sClient.Patch(context.Background(), patchedCluster, client.MergeFrom(cluster))
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("does not reconcile", func() {
+			result, err := reconciler.Reconcile(ctx, request)
+
+			Expect(result.Requeue).To(BeFalse())
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(ramClient.ApplyResourceShareCallCount()).To(Equal(0))
+		})
+	})
+
+	When("the transit gateway is in the same account as the cluster", func() {
+		BeforeEach(func() {
+			patchedIdentity := clusterIdentity.DeepCopy()
+			patchedIdentity.Spec.RoleArn = fmt.Sprintf("arn:aws:iam::%s:role/the-role-name", sourceAccountID)
+			err := k8sClient.Patch(context.Background(), patchedIdentity, client.MergeFrom(clusterIdentity))
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("does not reconcile", func() {
+			result, err := reconciler.Reconcile(ctx, request)
+
+			Expect(result.Requeue).To(BeFalse())
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(ramClient.ApplyResourceShareCallCount()).To(Equal(0))
+		})
+	})
+
+	When("sharing the transit gateway fails", func() {
+		BeforeEach(func() {
+			ramClient.ApplyResourceShareReturns(errors.New("boom"))
+		})
+
+		It("returns an error", func() {
+			_, err := reconciler.Reconcile(ctx, request)
+			Expect(err).To(MatchError(ContainSubstring("boom")))
+		})
+	})
+
+	When("the cluster no longer exists", func() {
+		BeforeEach(func() {
+			request.Name = "does-not-exist"
+		})
+
+		It("does not reconcile", func() {
+			result, err := reconciler.Reconcile(ctx, request)
+
+			Expect(result.Requeue).To(BeFalse())
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(ramClient.ApplyResourceShareCallCount()).To(Equal(0))
+		})
+	})
+
+	When("getting the cluster returns an error", func() {
+		BeforeEach(func() {
+			fakeClusterClient := new(controllersfakes.FakeClusterClient)
+			fakeClusterClient.GetReturns(nil, errors.New("boom"))
+			reconciler = controllers.NewShareReconciler(fakeClusterClient, ramClient)
+		})
+
+		It("returns an error", func() {
+			_, err := reconciler.Reconcile(ctx, request)
+			Expect(err).To(MatchError(ContainSubstring("boom")))
+		})
+	})
+
+	When("the transit gateway arn annotation is invalid", func() {
+		BeforeEach(func() {
+			patchedCluster := cluster.DeepCopy()
+			patchedCluster.Annotations[annotations.NetworkTopologyTransitGatewayIDAnnotation] = "not:a:valid/arn"
+			err := k8sClient.Patch(context.Background(), patchedCluster, client.MergeFrom(cluster))
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("returns an error", func() {
+			_, err := reconciler.Reconcile(ctx, request)
+			Expect(err).To(HaveOccurred())
+		})
+	})
+
+	When("the AWSCluster does not exist", func() {
+		BeforeEach(func() {
+			patchedCluster := cluster.DeepCopy()
+			patchedCluster.Spec.InfrastructureRef = &corev1.ObjectReference{
+				Kind:      "AWSCluster",
+				Namespace: namespace,
+				Name:      "does-not-exist",
+			}
+			err := k8sClient.Patch(context.Background(), patchedCluster, client.MergeFrom(cluster))
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("returns an error", func() {
+			_, err := reconciler.Reconcile(ctx, request)
+			Expect(err).To(HaveOccurred())
+		})
+	})
+
+	When("the AWSClusterRoleIdentity does not exist", func() {
+		BeforeEach(func() {
+			patchedCluster := awsCluster.DeepCopy()
+			patchedCluster.Spec.IdentityRef = &capa.AWSIdentityReference{
+				Kind: "AWSClusterRoleIdentity",
+				Name: "does-not-exist",
+			}
+			err := k8sClient.Patch(context.Background(), patchedCluster, client.MergeFrom(awsCluster))
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("returns an error", func() {
+			_, err := reconciler.Reconcile(ctx, request)
+			Expect(err).To(HaveOccurred())
+		})
+	})
+
+	When("the AWSClusterRoleIdentity ARN is invalid", func() {
+		BeforeEach(func() {
+			patchedIdentity := clusterIdentity.DeepCopy()
+			patchedIdentity.Spec.RoleArn = "not:a:valid/arn"
+			err := k8sClient.Patch(context.Background(), patchedIdentity, client.MergeFrom(clusterIdentity))
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("returns an error", func() {
+			_, err := reconciler.Reconcile(ctx, request)
+			Expect(err).To(HaveOccurred())
+		})
+	})
+})
