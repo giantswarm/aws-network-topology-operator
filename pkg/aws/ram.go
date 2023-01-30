32dc863e7a69d707ad64af8ab6eab41f62216d75
Mario Nitchev <mario@giantswarm.io>
2023-01-30 10:08:41 +0200

    Finish integration tests

--- pkg/aws/ram.go
+++ pkg/aws/ram.go
@@ -3,9 +3,15 @@ package aws
 import (
 	"context"
 
+	"github.com/aws/aws-sdk-go/aws"
 	awssdk "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ram"
+	"github.com/go-logr/logr"
+	"github.com/pkg/errors"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
+
+const ResourceOwnerSelf = "SELF"
 
 type ResourceShare struct {
 	Name              string
@@ -22,11 +28,41 @@ func NewRAMClient(ramClient *ram.RAM) *RAMClient {
 }
 
 func (c *RAMClient) ApplyResourceShare(ctx context.Context, share ResourceShare) error {
-	_, err := c.ramClient.CreateResourceShare(&ram.CreateResourceShareInput{
+	logger := c.getLogger(ctx, share)
+
+	resourceShare, err := c.ramClient.GetResourceShares(&ram.GetResourceSharesInput{
+		Name:          aws.String(share.Name),
+		ResourceOwner: aws.String(ResourceOwnerSelf),
+	})
+	if err != nil {
+		logger.Error(err, "failed to get resource share")
+		return errors.WithStack(err)
+	}
+
+	if len(resourceShare.ResourceShares) != 0 {
+		logger.Info("resource share already exists")
+		return nil
+	}
+
+	logger.Info("creating resource share")
+	_, err = c.ramClient.CreateResourceShare(&ram.CreateResourceShareInput{
 		AllowExternalPrincipals: awssdk.Bool(true),
 		Name:                    awssdk.String(share.Name),
 		Principals:              []*string{awssdk.String(share.ExternalAccountID)},
 		ResourceArns:            awssdk.StringSlice(share.ResourceArns),
 	})
-	return err
+
+	if err != nil {
+		logger.Error(err, "failed to create resource share")
+		return err
+	}
+
+	return nil
+}
+
+func (c *RAMClient) getLogger(ctx context.Context, share ResourceShare) logr.Logger {
+	logger := log.FromContext(ctx)
+	logger = logger.WithName("ram-client")
+	logger = logger.WithValues("resource-share-name", share.Name, "resource-arns", share.ResourceArns)
+	return logger
 }
